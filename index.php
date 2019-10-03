f8c03d072b67fc6b9a1a2bab0db4c088ead62151
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2019-10-03 12:15:40 +0200

    Test accès dossier

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-echo "test";
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<ul>
+    <li><a href="Discussions/TD1.php">Discussion</a></li>
+    <li><a href="identification/login.php">Login</a></li>
+    <li><a href="identification/sign_up.php">Login</a></li>
+</ul>
 
+</body>
+</html>
