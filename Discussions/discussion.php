98c380d46f5500cb94afa35e785ec41c72a2a3bc
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2019-10-03 12:08:13 +0200

    Discussion le début

--- Discussions/discussion.php
+++ Discussions/discussion.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Titre</title>
+    <title>Discussion</title>
 </head>
 <body>
+<form action="data-processing.php" method="post">
+    <label for="message"> Écrivez les mots souhaités (2 Maximum) : </label><br>
+    <input type="text" name="message" id="message" value="Words">
+    <input type="button" name="Submit" id="Submit" value="Send"
+</body>
+</html>
