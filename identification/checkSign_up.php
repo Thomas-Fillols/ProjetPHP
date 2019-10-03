499dbc17a9cd5df15b58401366272d97b73e3b1a
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-03 15:14:29 +0200

    page de login et verification de login fini

--- identification/checkSign_up.php
+++ identification/checkSign_up.php
@@ -6,12 +6,12 @@
         or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
 
 
+    $utilisateur = $_POST['identifiant'];
+    $mdp = $_POST['password'];
+    $email = $_POST['email'];
+    $role = $_POST['role'];
 
-
-    $utilisateur='thomas';
-    $mdp='zawarudo';
-    $email='tfillols@yahoo.fr';
-    $role='0';
+    echo "test<br/>";
 
     $query='INSERT INTO utilisateur(pseudo,password,email,role)VALUES(';
     $query.='"'.$utilisateur.'",';
@@ -19,12 +19,12 @@
     $query.='"'.$email.'",';
     $query.='"'.$role.'")';
 
-    /*
     if(!($dbResult=mysqli_query($dbLink, $query))){
-            echo'Erreur de requête<br/>';
-            //Affiche le type d'erreur.
-            echo'Erreur:'.mysqli_error($dbLink).'<br/>';
-            //Affiche la requête envoyée.
-            echo'Requête:'.$query.'<br/>';
-            exit();
-        }*/
+        echo'Erreur de requête<br/>';
+        //Affiche le type d'erreur.
+        echo'Erreur:'.mysqli_error($dbLink).'<br/>';
+        //Affiche la requête envoyée.
+        echo'Requête:'.$query.'<br/>';
+        exit();
+    }
+
--- identification/login.php
+++ identification/login.php
@@ -2,21 +2,21 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>PAGGGGEEEEEEE</title>
+    <title>Login</title>
 </head>
 <body>
 
 
+<!--------------------------------------------------->
 <!---------formulaire d'identification--------------->
 
-<form action="check.php" method="post">
+<form action="checkLogin.php" method="post">
     <input type="text" name="identifiant"/>Nom ou Pseudo<br/>
 
     <input type="password" name="mdp">Mot de passe<br/>
 
-    <input type="submit" name="action" value="mailer">
+    <input type="submit" name="action" value="connection">
 </form>
 
 </body>
 </html>
-
