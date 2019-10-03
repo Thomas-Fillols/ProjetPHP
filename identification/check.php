cee201ca5c866a30aec7e3fe1d80b56e7ba4a0ff
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-03 14:46:59 +0200

    creation, connection et verification de la base de donnee

--- identification/check.php
+++ identification/check.php
@@ -1,1 +1,30 @@
 <?php
+    $dbLink=mysqli_connect('mysql-freenote.alwaysdata.net', 'freenote','zawarudo')
+        or die('Erreur de connexion au serveur:'.mysqli_connect_error());
+
+    mysqli_select_db($dbLink,'freenote_sql')
+        or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
+
+
+
+
+    $utilisateur='thomas';
+    $mdp='zawarudo';
+    $email='tfillols@yahoo.fr';
+    $role='0';
+
+    $query='INSERT INTO utilisateur(pseudo,password,email,role)VALUES(';
+    $query.='"'.$utilisateur.'",';
+    $query.='"'.$mdp.'",';
+    $query.='"'.$email.'",';
+    $query.='"'.$role.'")';
+
+    /*
+    if(!($dbResult=mysqli_query($dbLink, $query))){
+            echo'Erreur de requête<br/>';
+            //Affiche le type d'erreur.
+            echo'Erreur:'.mysqli_error($dbLink).'<br/>';
+            //Affiche la requête envoyée.
+            echo'Requête:'.$query.'<br/>';
+            exit();
+        }*/
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 echo "test";
+?>
 
+<html>
+    <a href="identification/login.php">login</a><br>
+    <a href="identification/sign_up.php">sign_up</a>
+</html>
