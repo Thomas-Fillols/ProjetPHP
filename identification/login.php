42e7116e5c08d028b82923ca6b11d025764f37f1
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-03 12:10:00 +0200

    creation des condition d'utilisation, de l'espace de verification et des formulaires d'inscription et connections

--- identification/login.php
+++ identification/login.php
@@ -9,15 +9,10 @@
 
 <!---------formulaire d'identification--------------->
 
-<form action="verification.php" method="post">
-    <input type="text" name="identifiant"/>Identifiant<br/><br/>
-
-    <input type="text" name="E-mail">E-mail<br/><br/>
+<form action="check.php" method="post">
+    <input type="text" name="identifiant"/>Nom ou Pseudo<br/>
 
     <input type="password" name="mdp">Mot de passe<br/>
-    <input type="password" name="mdp">Verification de mot de passe<br/><br/>
-
-    <input type="text" name="téléphone">Numero de téléphone<br/><br/>
 
     <input type="submit" name="action" value="mailer">
 </form>
--- identification/sign_up.php
+++ identification/sign_up.php
@@ -1,1 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>PAGGGGEEEEEEE</title>
+</head>
+<body>
+
+<form action=".php" method="post">
+
+    <input type="text" name="identifiant"/>Nom ou Pseudo<br/><br/>
+
+    <input type="text" name="email">E-mail<br/><br/>
+
+    <input type="password" name="mdp">Mot de passe<br/>
+    <input type="password" name="mdp">Verification de mot de passe<br/><br/>
+
+    <input type="checkbox" name="condition">Condition d'utilisation<br/>
+    <a href="../conditionG/condition.php">Condition général d'utilisation</a>
+
+    <input type="submit" name="action" value="mailer">
+</form>
+
+</body>
+</html>
