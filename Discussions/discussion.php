0f455db127fc46c7f42bf99d305b569dc0176402
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2019-10-09 11:08:58 +0200

    Envoie message + vérif 2 mots max + Close(?)

--- Discussions/discussion.php
+++ Discussions/discussion.php
@@ -7,6 +7,9 @@
 <form action="Check_message.php" method="post">
     <label for="Participation"> Écrivez les mots souhaités (2 Maximum) : </label><br>
     <input type="Text" name="Participation" id="message" value="Écrivez votre message">
-    <input type="Submit" name="Submit" id="Submit" value="Send"
+    <input type="Submit" name="BPart" id="BParticipation" value="Send"><br>
+    <label for="Close"> Fermez la discussion:</label>
+    <input type="submit" name="CloseDisc" id="Close" value="Close Discussion">
+
 </body>
 </html>
