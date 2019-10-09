005ee88e2f2ed0d516e21c0b165b3dbf783b402a
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-09 17:21:34 +0200

    récuperation de toutes le donnees de la bd mise a part le mot de passe

--- myprofil.php
+++ myprofil.php
@@ -2,13 +2,43 @@
 
 session_start();
 
-if (isset($_SESSION['login']) && isset($_SESSION['password'])) {
+if (isset($_SESSION['pseudo']) && isset($_SESSION['password'])) {            // On teste pour voir si nos variables ont bien été enregistrées
 
-    // On teste pour voir si nos variables ont bien été enregistrées
+    $dbLink=mysqli_connect('mysql-freenote.alwaysdata.net', 'freenote','zawarudo')
+        or die('Erreur de connexion au serveur:'.mysqli_connect_error());
 
+    mysqli_select_db($dbLink,'freenote_sql')
+        or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
 
-    echo 'Votre login est '.$_SESSION['login'].' et votre mot de passe est '.$_SESSION['password'].'.';
+    $pseudo = $_SESSION['pseudo'];
+    $password = $_SESSION['password'];
+
+    $query="SELECT pseudo,email,password,role FROM utilisateur WHERE pseudo = '$pseudo' AND password = '$password'";
+
+    if(!($dbResult=mysqli_query($dbLink, $query))){
+        echo'Erreur de requête<br/>';
+        //Affiche le type d'erreur.
+        echo'Erreur:'.mysqli_error($dbLink).'<br/>';
+        //Affiche la requête envoyée.
+        echo'Requête:'.$query.'<br/>';
+        exit();
+    }
+
+    $dbRow=mysqli_fetch_assoc($dbResult);
+
+    if ($dbRow['role'] == 0){
+        $role = 'Membre';
+    }else{
+        $role = 'Super-administrateur';
+    }
+
+    echo 'Votre login est '.$_SESSION['pseudo'].'.';
     echo '<br />';
+    echo 'Votre E-mail est : '.$dbRow['email'];
+    echo '<br />';
+    echo 'Votre rôle est : '.$role;
+    echo '<br />';
+
 
     // On affiche un lien pour fermer notre session
     echo '<a href="identification/logout.php">Déconnection</a>';
