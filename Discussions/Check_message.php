0ab44c796bd2d5105a921986987ae14465abd7b5
Lucas Bracq <lucas.bracq@etu.univ-amu.fr>
2019-10-09 11:10:57 +0200

    Envoie message + vérif 2 mots max + Close(?)

--- Discussions/Check_message.php
+++ Discussions/Check_message.php
@@ -1,9 +1,9 @@
 <?php
 
 $Participation = $_POST['Participation'];
-$Submit = $_POST['Submit'];
+$Submit = $_POST['BPart'];
+$Close = $_POST['CloseDisc'];
 $NbMots = explode(" ", $Participation);
-var_dump($NbMots);
 $dbLink = mysqli_connect('mysql-freenote.alwaysdata.net', 'freenote', 'zawarudo')
 or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 mysqli_select_db($dbLink, 'freenote_sql')
@@ -18,25 +18,40 @@ if ($Submit == 'Send' && sizeof($NbMots)<=2) {
               <title>Message envoyé</title>
               </head>
               <body>
-              <header> Bonjour votre participation a bien été envoyée. </header>
+              <header> Votre participation a bien été enregistrée. </header>
               <ul>
               </ul></body>' . PHP_EOL;
     $query = 'INSERT INTO Discussion(Participation)VALUES(';
     $query .= '"' . $Participation . '")';
 }
-    else if (sizeof($NbMots)>2){
-        throw (new Exception('Vous voulez envoyer trop de mots'));
-    }
+else if (sizeof($NbMots)>2)
+    throw (new Exception('Vous voulez envoyer trop de mots'));
 
+else if (sizeof($Participation)>60)
+    throw (new Exception('Trop de caractères'));
 
-    if (!($dbResult = mysqli_query($dbLink, $query))) {
-        echo 'Erreur de requête<br/>';
-        // Affiche le type d'erreur.
-        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-        // Affiche la requête envoyée.
-        echo 'Requête : ' . $query . '<br/>';
-        exit();
-    }
-    else {
-    echo '<br/><strong> Bouton non géré!</strong>';
+if (!($dbResult = mysqli_query($dbLink, $query))) {
+    echo 'Erreur de requête<br/>';
+    // Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+    // Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
 }
+
+if ($Close == 'Close Discussion') {
+    echo '<!DOCTYPE html> 
+              <html lang="fr">
+              <head>
+              <title>Discussion fermée</title>
+              </head>
+              <body>
+              <header> La discussion a bien été fermée </header>
+              <ul>
+              </ul></body>' . PHP_EOL;
+    $query = 'INSERT INTO Discussion(Discussion)VALUES(';
+    $query .= '"' . $Close . '")';
+}
+
+if($Submit=! $_POST['Send'] || $Close=! $_POST[('Close Discussion')])
+    echo '<br/><strong> Bouton non géré!</strong>';
