d6bc38ccdc64f2cfc3bd53a15300c2e9a827e207
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-10 09:58:46 +0200

    ajout de modification de mail

---
+++ identification/changeMail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Réinitialisation de MDP</title>
+</head>
+<body>
+
+
+<!---------------------------------------------------------------->
+<!---------formulaire de changement de mot de passe--------------->
+
+<form action="checkChangeMail.php" method="post">
+    <p>Entrez votre nouveau mail :</p>
+    <input type="text" name="email"/><br/>
+
+    <input type="submit" name="action" value="connection">
+</form>
+
+</body>
+</html>
---
+++ identification/checkChangeMail.php
@@ -0,0 +1,28 @@
+<?php
+
+    session_start();
+
+    $dbLink=mysqli_connect('mysql-freenote.alwaysdata.net', 'freenote','zawarudo')
+        or die('Erreur de connexion au serveur:'.mysqli_connect_error());
+
+    mysqli_select_db($dbLink,'freenote_sql')
+        or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
+
+    if(isset($_POST['email'])) {
+        $email = $_POST['email'];
+    }
+
+    $pseudo = $_SESSION['pseudo'];
+
+    $query="UPDATE utilisateur SET email ='$email' WHERE pseudo = '$pseudo'";
+
+    if(!($dbResult=mysqli_query($dbLink, $query))) {
+        echo 'Erreur de requête<br/>';
+        //Affiche le type d'erreur.
+        echo 'Erreur:' . mysqli_error($dbLink) . '<br/>';
+        //Affiche la requête envoyée.
+        echo 'Requête:' . $query . '<br/>';
+        exit();
+    }
+
+    header('Location:myprofil.php');
