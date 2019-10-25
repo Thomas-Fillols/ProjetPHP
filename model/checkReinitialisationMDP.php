ed1ced14c130d9db94e937ace98ee8a7b70f7224
f17018633 <thomas.fillols@etu.univ-amu.fr>
2019-10-25 16:43:15 +0200

    pk le check reinitialisation ne marche pas

--- model/checkReinitialisationMDP.php
+++ model/checkReinitialisationMDP.php
@@ -1,11 +1,12 @@
 <?php
+    require ('../toolclass/function.inc.php');
+
 
     if(isset($_POST['MDP'])) {
         $MDP = md5($_POST['MDP']);
     }
 
-    $pseudo = $_SESSION['pseudo'];
 
-    $query="UPDATE utilisateur SET utilisateur.password ='$MDP' WHERE pseudo = '$pseudo'";
+    $query="UPDATE utilisateur SET utilisateur.password ='$MDP' WHERE pseudo ='$pseudo'";
 
     access_bd($dbLink,$query);
